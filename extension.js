523f33814a694543ef836c7ee0d084764e176053
edogboss <edogboss@gmail.com>
2015-01-16 22:00:11 -0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -52,7 +52,7 @@
 
     //Change the bots default settings and make sure they are loaded on launch
 
-    localStorage.setItem("basicBotsettings", JSON.stringify({
+    localStorage.setItem("ObbyBotsettings", JSON.stringify({
         botName: "ObbyBot",
         language: "english",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
@@ -105,6 +105,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
+    $.getScript('https://github.com/edogboss/basicBot-customization/tree/patch-1', extend);
 
 }).call(this);
